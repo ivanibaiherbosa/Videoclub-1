57be417f2cbb04fa0002b4cd0070539160c00edc
Iván Ibai <ivanibaiherbosa@gmail.com>
2024-10-29 13:13:23 +0100

    añado funcionalidades a Cliente.php y añado getter a un atributo privado para mostrarlo en Inicio.php

--- Cliente.php
+++ Cliente.php
@@ -4,18 +4,23 @@ class Cliente {
 
     public string $nombre;
     private int $numero;
-    private array $soportesAlquilados = [];
-    private int $numSoportesAlquilados = 0;
+    private array $soportesAlquilados = []; // array para almacenar los soportes que ha alquilado
+    private int $numSoportesAlquilados = 0; // contador de alquileres realizados por elñ cliente
     private int $maxAlquilerConcurrente;
     
+// constructor que a diferencia de Cintavideo, Dvd y Juego NO está sobreescrito
+// ya que eso eran soportes y el cliente va a tener su propio constructor porque es un objeto distinto
+
 public function __construct(string $nombre, int $numero,
- int $maxAlquilerConcurrente = 3) {
+ int $maxAlquilerConcurrente = 3) {    // maxAlquilerConcurrente para este cliente es 3 pero esto se puede modificar segun cliente
 
 $this->nombre = $nombre;
 $this->numero = $numero;
 $this->maxAlquilerConcurrente = $maxAlquilerConcurrente;
 
  }
+
+// Getter y setter de numero, de nombre no hace falta porque es public 
 
 public function getNumero(): int {
     return $this->numero;
@@ -25,10 +30,21 @@ public function setNumero(int $numero): void {
     $this->numero = $numero;
 }
 
+// solamente getter de numSoportesAlquilados
+
 public function getNumSoportesAlquilados(): int {
     return $this->numSoportesAlquilados;
 }
 
+// maxAlquilerConcurrente es private asi que hago un getter para acceder 
+// en Inicio.php, no hago setter porque no necesito permitir cambios tras crear el objeto
+
+public function getMaxAlquilerConcurrente(): int {
+    return $this->maxAlquilerConcurrente;
+}
+
+
+// Método para alquilar un soporte
 
 public function alquilarSoporte(Soporte $soporte): void {
     if ($this->numSoportesAlquilados < $this->maxAlquilerConcurrente) {
@@ -41,9 +57,23 @@ public function alquilarSoporte(Soporte $soporte): void {
     }
 }
 
+//Recorrer array soportesAlquilados y comprobar si el soporte ya está alquilado (no se puede usar)
+
+public function tieneAlquilado(Soporte $S): bool {
+    foreach ($this->soportesAlquilados as $soporteAlquilado) {
+        if ($soporteAlquilado === $S) {
+            return true; // si encuentra el soporte te devuelve true
+        }
+    }
+
+    return false; // si no encuentra el soporte en soportesAlquilados devuelve false
+}
+
+// muestraResumen() para que en inicio.php aparezca el resumen del cliente elegido
+
 public function muestraResumen(): void {
-    echo "cliente: $this->nombre <br>";
-    echo "Numero: $this->numero <br>";
+    echo "cliente: " . $this->nombre . "<br>";
+    echo "Numero: " .  $this->getNumero() . "<br>";
     echo "Total de alquileres: " . count($this->soportesAlquilados) . "<br>";
 }
 
--- inicio.php
+++ inicio.php
@@ -3,11 +3,14 @@ include_once "Soporte.php";
 include_once "CintaVideo.php";
 include_once "Dvd.php";
 include_once "Juego.php";
+include_once "Cliente.php";
 
 $soporte1 = new Soporte("Tenet", 22, 3);
 $miCinta = new CintaVideo("los cazafantasmas", 23, 3.5, 107);
 $miDvd = new Dvd("Origen", 24, 15, "es, en, fr", "16:9");
 $miJuego = new Juego("the last of us", 26, 49, "ps4", 1, 1);
+
+$cliente = new cliente("Iván", 1, 5); // en este caso especial maxAlquilerConcurrente no es el valor por defecto (3) y es 5
 
 echo  $soporte1->titulo;
 
@@ -16,6 +19,8 @@ echo  "<br> precio: " . $soporte1->getPrecio() . " euros";
 echo "<br> precio iva incluido: " . $soporte1->getPrecioConIva() . " euros"; ;
  
 echo "<br> el resumen es: " . $soporte1->muestraResumen();
+
+echo "<br> ----------------------------------------------";
 
 //----------------------------------------------
 
@@ -27,6 +32,8 @@ echo "<br> precio iva incluido: " . $miCinta->getPrecioConIva() . " euros";
 
 echo "<br> el resumen es: " . $miCinta->muestraResumen();
 
+echo "<br> ----------------------------------------------";
+
 //-----------------------------------------------
  
 echo "<br>". $miDvd->titulo;
@@ -37,6 +44,8 @@ echo "<br> precio iva incluido: " . $miDvd->getPrecioConIva() . " euros";
 
 echo "<br> " . $miDvd->muestraResumen();
 
+echo "<br> -----------------------------------------------";
+
 //------------------------------------------------
 
 echo "<br>". $miJuego->titulo;
@@ -45,11 +54,24 @@ echo "<br> Precio: " . $miJuego->getPrecio(). " euros";
 
 echo "<br> precio con iva incluido: " . $miJuego->getPrecioConIva()."euros";
 
+echo "<br> jugadores posibles: ". $miJuego->muestraJugadoresPosibles();
+
 echo "<br> ". $miJuego->muestraResumen();
 
-echo "<br> jugadores posibles: ". $miJuego->muestraJugadoresPosibles();
+echo "<br> -----------------------------------------------";
 
 //------------------------------------------------
 
+echo "<br>Cliente número: " . $cliente->getNumero();
 
- ?>
+echo "<br>Nombre: " . $cliente->nombre;
+
+echo "<br>Max Alquiler Concurrente: " . $cliente->getMaxAlquilerConcurrente();
+
+echo "<br>Resumen del Cliente: <br>";
+
+$cliente->muestraResumen();
+
+echo "<br>-----------------------------------------------";
+
+?>
